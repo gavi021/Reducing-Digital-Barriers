f5db00c5aa93f2fb76eafc15decb29345e154448
Gavriella Santiago <124306874+gavi021@users.noreply.github.com>
2025-12-16 14:52:03 -0700

    Add files via upload

---
+++ Thesis/east/contact.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <div class="content">
+        <head>
+            <title>Gavi's Contact Information</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="author" content="Gavriella Santiago">
+            <meta name="description" content="A page designated to different ways to contact me, email or LinkedIn">
+            <meta name="keywords" content="HTML,CSS,JavaScript">
+            <link rel="stylesheet" type="text/css" href="style.css">           
+        </head>
+        <body>
+            <div class="title">
+                <header>
+                    <h1>Contact Information</h1>
+                </header>
+            </div>
+            <div class="topnav">
+                <a href="https://cs.colostate.edu:4444/~gavi021/east/index.php">Home</a>
+                <a href="https://cs.colostate.edu:4444/~gavi021/east/about.php">About Me</a>
+                <a href="https://cs.colostate.edu:4444/~gavi021/east/contact.php"> Contact Information</a>
+                <a href="https://cs.colostate.edu:4444/~gavi021/west/contact.php">To the West</a>
+                <a href="https://cs.colostate.edu:4444/~gavi021/contact.html">To the Center</a>
+            </div>
+
+            <div class="bottombar">
+                <footer>
+                    <p>Look out for my contact page if you have questions!</p> 
+                </footer>
+            </div>
+            <img src="./images/butterfly.jpg" alt="Purple Sky" style="width: 250px" align ="right">
+            <div id="main">
+                <h2>If You Want to Contact Me:</h2>
+                    <h3>Email</h3>
+                        <p>
+                        <em>School Email:</em> 
+                        gavi021@colostate.edu
+                        </p>
+                    <h3>Social Media</h3>
+                        <p>
+                            <em>LinkedIn:</em>
+                            <a href="https://www.linkedin.com/in/gavriella-santiago-b2b014221/"
+                            target"_blank">Gavriella Santiago</a>
+                        </p>
+                <h2>More About Contacting Me:</h2>
+                <p>
+                <blockquote cite="https://www.lipsum.com/">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi augue libero, congue non ultrices vitae, maximus
+                    quis nunc. Praesent ultricies lorem leo, vel hendrerit nisi hendrerit a. Etiam finibus lectus eu egestas porttitor.
+                    Proin viverra ante a dolor hendrerit, ut auctor urna euismod. Mauris ipsum nibh, laoreet vitae velit ut, aliquam
+                    fermentum diam. In nec luctus enim. In ultrices libero a libero scelerisque euismod. Praesent lorem ipsum, sollicitudin
+                    vitae venenatis vel, blandit sit amet ante. Nullam ex purus, accumsan non rutrum nec, luctus quis erat. Mauris 
+                    nec mattis felis. Integer eu efficitur enim, ac egestas sem. Orci varius natoque penatibus et magnis dis parturient montes, 
+                    nascetur ridiculus mus. Etiam vitae lorem ante.
+                    <br>
+                    Pellentesque tincidunt, tellus sit amet ullamcorper aliquam, massa lectus accumsan nulla, sed auctor neque sapien at est. 
+                    Morbi quam urna, feugiat ac ligula id, maximus tincidunt lacus. Vivamus nec ipsum vitae libero tincidunt volutpat in et eros. 
+                    Nullam fringilla massa orci, a maximus lorem mattis vitae. Ut maximus dolor ut tortor tempor, ac tristique mauris 
+                    scelerisque. Phasellus malesuada condimentum pretium. Quisque dapibus imperdiet eleifend. Vestibulum elementum libero 
+                    vel sapien dapibus cursus. Praesent posuere lacinia mi non condimentum. Cras orci nunc, congue in leo nec, egestas imperdiet 
+                    augue. Aliquam gravida at arcu at mattis. Pellentesque fermentum erat risus, eu semper mi sagittis ut. Fusce fringilla, 
+                    tortor eget fringilla rhoncus, nibh magna interdum dolor, ac accumsan lorem purus non sapien. Nam non libero mattis nisl 
+                    pretium eleifend at ac est. Morbi semper, justo id eleifend mattis, lacus augue rhoncus nisi, sed ullamcorper lacus tellus 
+                    sed ipsum. Suspendisse eu enim eu ex dictum feugiat vitae in mi.
+                </blockquote>
+                </p>
+            </div>
+        </body>
+    </div>
+</html>
---
+++ Thesis/west/about.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Gavi's About Me</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="Gavriella Santiago">
+        <meta name="description" content="About me page where I talk about my education and hobbies">
+        <meta name="keywords" content="HTML,CSS,JavaScript">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <div class="title">
+        <header>
+            <h1>About Me</h1>
+        </header>
+    </div>
+    <body>
+        <div class="topnav">
+            <a href="https://cs.colostate.edu:4444/~gavi021/west/index.php">Home</a>
+            <a href="https://cs.colostate.edu:4444/~gavi021/west/about.php">About Me</a>
+            <a href="https://cs.colostate.edu:4444/~gavi021/west/contact.php"> Contact Information</a>
+            <a href="https://cs.colostate.edu:4444/~gavi021/east/about.php">To the East</a>
+            <a href="https://cs.colostate.edu:4444/~gavi021/about.html">To the Center</a>
+        </div>
+        <div class="bottombar">
+            <footer>
+                <p>Look out for my contact page if you have questions!</p> 
+            </footer>
+        </div>
+        <div id="main">
+            <h2>Some More About Me</h2>
+                <h3>Education</h3>
+                    <p> I am currently a senior in computer science hoping to graduate this December. I began college 
+                        majoring in neuroscience, which soon turned into biochemistry. After taking a class in data science 
+                        I found that I really enjoyed coding, and made the switch to computer science. After college, I hope 
+                        to apply everything I've learned to the medical field focusing on working with different machines 
+                        within hospitals or other medical centers.
+                        <blockquote cite="https://www.lipsum.com/">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat tincidunt sollicitudin. 
+                        Aliquam erat volutpat. Vestibulum mattis blandit diam sed facilisis. 
+                        Integer nisl nisl, cursus a semper vitae, tempus nec erat. Nullam tellus orci, 
+                        imperdiet ut consectetur posuere, blandit in eros. Suspendisse potenti. Suspendisse quis pulvinar sapien. 
+                        Integer sit amet mattis tellus, quis egestas metus.
+                        Nam ut iaculis lacus. Duis ultrices ante vel velit sodales luctus.
+                        </blockquote>
+                    </p>
+                    <p>Source: <a href="https://www.lipsum.com/">Lipsum.com</a></p>
+                    </p>
+                <img src="./images/skii.jpg" alt="Ski Picture" style="width: 300px" align ="right">
+            <h3>Hobbies</h3>
+                <p>Some things I enjoy doing:
+                    <ul>
+                        <li>Trying out different crafts, sometimes during craft nights with friends</li>
+                        <li>Reading in my freetime or while on the bus</li>   
+                        <li>About two years ago I started to learn how to ski, and love going on the weekends</li>
+                        <li>Spending time with friends, usually shopping, baking, or watching movies</li>
+                    </ul>
+                </p>
+        </div>
+    </body>
+</html>
