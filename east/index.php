7fa57e0819a99b66e4bfd010759a2a34b5cf57f8
Gavriella Santiago <gavi021@colostate.edu>
2025-12-16 15:13:08 -0700

    Move to main

---
+++ east/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <div class="content">
+        <head>
+            <title>Gavi's Homepage</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="author" content="Gavriella Santiago">
+            <meta name="description" content="First assignment for CS312 where we begin creating a homepage about ourselves.">
+            <meta name="keywords" content="HTML,CSS,JavaScript">
+            <link rel="stylesheet" type="text/css" href="style.css">           
+        </head>
+        <body>
+            <div class="title">
+                <header>
+                    <h1>Gavi's Student Homepage</h1>
+                </header>
+            </div>
+
+            <div class="topnav">
+                <a href="https://cs.colostate.edu:4444/~gavi021/east/index.php">Home</a>
+                <a href="https://cs.colostate.edu:4444/~gavi021/east/about.php">About Me</a>
+                <a href="https://cs.colostate.edu:4444/~gavi021/east/contact.php">Contact Information</a>
+                <a href="https://cs.colostate.edu:4444/~gavi021/west/index.php">To the West</a>
+                <a href="https://cs.colostate.edu:4444/~gavi021/index.html">To the Center</a>
+            </div>
+
+            <div class="bottombar">
+                <footer>
+                    <p>Look out for my contact page if you have questions!</p> 
+                </footer>
+            </div>
+            <img src="./images/gavi-east.jpg" alt="Gavi - Banana Sign" style="width: 300px" align ="right">
+
+            <div id="main">
+                <h2>Welcome to my homepage!!</h2>
+                <p>
+                Welcome to my personal webpage! This page contains links to various sections about my interests, background, 
+                and hobbies. Feel free to click on any of the links above to learn more about me and what I do. Whether 
+                you're curious about my professional achievements, my favorite activities, or just want to learn a 
+                bit more about who I am, you're in the right place! I enjoy sharing different aspects of my life and passions, 
+                and I hope you'll find something that resonates with you.
+                </p>
+                <p>
+                <blockquote cite="https://www.lipsum.com/">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat tincidunt sollicitudin. 
+                Aliquam erat volutpat. Vestibulum mattis blandit diam sed facilisis. 
+                Integer nisl nisl, cursus a semper vitae, tempus nec erat. Nullam tellus orci, 
+                imperdiet ut consectetur posuere, blandit in eros. Suspendisse potenti. Suspendisse quis pulvinar sapien. 
+                Integer sit amet mattis tellus, quis egestas metus.
+                Nam ut iaculis lacus. Duis ultrices ante vel velit sodales luctus.
+                </blockquote>
+                </p>
+            </div>
+        </body>
+    </div>
+</html>
